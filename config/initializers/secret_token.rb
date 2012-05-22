71d25f898df3fafd00e63ba22ecf7fb6e287d640
Dave Paola <dpaola2@gmail.com>
2012-05-23 09:30:00 +1000

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthTracker::Application.config.secret_token = '9e96cab41a0b09919556657c1ab40727fb08702e71ec2fb4991043c231384ac9974770a1c08411a901ac5ef47e3264209cdb8ed7b0ba7908a0757d050777190d'
