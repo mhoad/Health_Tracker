fa576502359a2fdb438e46361fa96e4ef368aaa0
Dave Paola <dpaola2@gmail.com>
2012-05-24 01:17:30 +1000

    FB API Keys for staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,9 @@ HealthTracker::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+# API Keys 
+  FACEBOOK_APP_ID="223186147784111"
+  FACEBOOK_APP_SECRET="22acf16bd008c292a7861b2d437cce55"
+
 end
